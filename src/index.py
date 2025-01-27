d1e44aa2bd610a11b8a078e2229f40f2a5f774c1
baesik <0730bss@gmail.com>
2025-01-27 17:49:31 +0900

    Add: 이미지 파일 이름을 정렬하여 일관된 인덱싱 보장

--- src/index.py
+++ src/index.py
@@ -4,12 +4,12 @@ import sys
 
 
 script_path = os.path.dirname(os.path.realpath(__file__))
-IMAGE_INDEX_PATH = os.path.join(script_path, '../data/image_index.json')
 
 
-def generate_image_index(image_path):
+def generate_image_index(image_path, image_index_path):
     image_dir = os.fsencode(image_path)
     image_files = os.listdir(image_dir)
+    image_files = sorted(image_files, key=lambda x: os.fsdecode(x).lower())
     index_result = {}
     
     for i in range(len(image_files)):
@@ -17,12 +17,12 @@ def generate_image_index(image_path):
         if filename.endswith(".jpg") or filename.endswith(".JPEG"):
             index_result[i] = os.path.realpath(filename)
 
-    with open(IMAGE_INDEX_PATH, 'w') as json_file:
-        json.dump(index_result, json_file, indent=4)  # indent=4는 보기 좋게 들여쓰기를 추가하는 옵션입니다.
+    with open(image_index_path, 'w') as json_file:
+        json.dump(index_result, json_file, indent=4)
+        
 
-
-def get_image_path_by_index(index):
-    with open(IMAGE_INDEX_PATH, 'r') as json_file:
+def get_image_path_by_index(index, image_index_path):
+    with open(image_index_path, 'r') as json_file:
         index_data = json.load(json_file)
     
     return index_data[index]
@@ -30,5 +30,7 @@ def get_image_path_by_index(index):
 
 if __name__=="__main__":
     IMAGE_PATH = os.path.join(script_path, "../data/flickr30k/Images")
-    generate_image_index(IMAGE_PATH)
-    print(get_image_path_by_index('300'))
+    IMAGE_INDEX_PATH = os.path.join(script_path, '../data/image_index.json')
+
+    generate_image_index(IMAGE_PATH, IMAGE_INDEX_PATH)
+    print(get_image_path_by_index('300', IMAGE_INDEX_PATH))
